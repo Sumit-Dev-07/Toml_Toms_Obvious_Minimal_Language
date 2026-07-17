7ca34c71b02df52c7fc4c4858c9867b62ef2f0a5
Sumit Ojha <sumitojha264@gmail.com>
2026-07-17 11:58:35 +0530

    Setup Dagger Hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.devtools.ksp)
+    alias(libs.plugins.dagger.hilt.android)
 }
 
 android {
@@ -48,6 +50,10 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    // Dagger-Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
 
     // Unit Test
     testImplementation(libs.junit)
---
+++ app/src/main/java/com/app/toml/TomlApp.kt
@@ -0,0 +1,6 @@
+package com.app.toml
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TomlApp : Application()
